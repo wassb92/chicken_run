ae312f019a17eec3291379ce8c8598374a86fb5e
Wassini BOUZIDI <wassini.bouzidi@epitech.eu>
2021-05-20 13:09:11 +0200

    [MOD] Rename path

---
+++ chicken.js
@@ -0,0 +1,35 @@
+require('dotenv/config');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const postsRoute = require('./routes/posts');
+const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const OK =  200;
+const NOT_FOUND = 404;
+
+/* Routes */
+
+app.get('/', (req, res) => {
+    res.redirect('/chicken');
+});
+
+app.use('/chicken', postsRoute, (req, res) => {
+    res.status(OK);
+});
+
+app.get('*', (req, res) => {
+    res.status(NOT_FOUND).send('Not found, go to /chicken page');
+});
+
+
+/* Connect to DB */
+
+mongoose.connect(process.env.DB_CONNECTION ,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.listen(process.env.PORT);
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,11 @@ const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/db');
+const OK = 200;
+const NOT_FOUND = 404;
 
 router.get('/', async (req, res) => {
+    res.status(OK);
     try {
         const posts = await Post.find();    
         res.json(posts);
@@ -16,9 +19,7 @@ router.post('/', async (req, res) => {
     const post = new Post({
         name: req.body.name,
         birthday: req.body.birthday,
-        weight: req.body.weight,
-	    steps: req.body.steps,
-        isRunning: req.body.isRunning
+        weight: req.body.weight
     });
     try {
 	    const savedPost = await post.save()
@@ -28,28 +29,29 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/:postId', async (req, res) => {
+router.get('/:ChickenId', async (req, res) => {
     try {
-	    const post = await Post.findById(req.params.postId);
+	    const post = await Post.findById(req.params.ChickenId);
 	    res.json(post);
     } catch (err) {
         res.json({message: err});
+        res.status(NOT_FOUND);
     }
 });
 
-router.delete('/:postId', async (req, res) => {
+router.delete('/:ChickenId', async (req, res) => {
     try {
-	const removedPost = await Post.remove({_id: req.params.postId});
+	const removedPost = await Post.remove({_id: req.params.ChickenId});
         res.json(removedPost);
     } catch(err) {
         res.json({message: err});
     }
 });
 
-router.patch('/:postId', async (req, res) => {
+router.patch('/:ChickenId', async (req, res) => {
     try {
         const updatedPost = await Post.updateOne(
-            { _id: req.params.postId},
+            { _id: req.params.ChickenId},
             { $set: {
                 name: req.body.name,
                 birthday: req.body.birthday,
@@ -63,10 +65,10 @@ router.patch('/:postId', async (req, res) => {
     }
 });
 
-router.patch('/run/:postId', async (req, res) => {
+router.patch('/run/:ChickenId', async (req, res) => {
     try {
         const updatedPost = await Post.updateOne(
-            { _id: req.params.postId},
+            { _id: req.params.ChickenId},
             { $set: {
                 name: req.body.name,
                 birthday: req.body.birthday,
@@ -80,14 +82,13 @@ router.patch('/run/:postId', async (req, res) => {
     }
 });
 
-router.get('/run/:postId', async (req, res) => {
+router.get('/run/:ChickenId', async (req, res) => {
     try {
-	    const post = await Post.findById(req.params.postId);
+	    const post = await Post.findById(req.params.ChickenId);
 	    res.json(post);
     } catch (err) {
         res.json({message: err});
     }
 });
-
 
 module.exports = router;
